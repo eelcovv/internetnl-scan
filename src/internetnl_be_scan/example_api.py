0fc189346934e57345f6f622d4da946c7793c869
Eelco van Vliet <eelcovv@gmail.com>
2020-10-07 21:56:49 +0200

    opslaan naar sqlite lukt nu

--- src/internetnl_be_scan/example_api.py
+++ src/internetnl_be_scan/example_api.py
@@ -2,8 +2,10 @@ import sys
 import os
 import time
 import pickle
+import sqlite3
 import argparse
 import logging
+import glob
 import getpass
 from pathlib import Path
 from tqdm import trange
@@ -15,6 +17,12 @@ from internetnl_be_scan import __version__
 
 logging.basicConfig(format='%(asctime)s %(name)-12s - %(levelname)-8s : %(message)s')
 _logger = logging.getLogger()
+
+
+def make_cache_file_name(directory, scan_id):
+    """ build the cache file name """
+    cache_file_name = f"{scan_id}.pkl"
+    return directory / Path(cache_file_name)
 
 
 class Credentials(object):
@@ -55,17 +63,19 @@ class Credentials(object):
 class InternetNlScanner(object):
 
     def __init__(self,
-                 domains: list,
+                 urls_to_scan: list,
                  tracking_information: str = None,
                  scan_name: str = None,
                  scan_type: str = "web",
                  api_url: str = "https://batch.internet.nl/api/batch/v2/",
                  interval: int = 30,
                  cache_directory: str = "cache",
-                 scan_id: str = None
+                 ignore_cache: bool = True,
+                 output_filename: str = None
                  ):
 
         self.api_url = api_url
+        self.output_filename = output_filename
         if tracking_information is None:
             self.tracking_information = "{time}".format(time=time.time())
         else:
@@ -75,32 +85,31 @@ class InternetNlScanner(object):
         else:
             self.scan_name = scan_name
         self.scan_type = scan_type
-        self.domains = domains
+        self.urls_to_scan = urls_to_scan
 
         self.interval = interval
 
+        self.scan_id = None
+        self.domains = dict()
         self.response = None
         self.finished_scan = False
         self.scan_results: object = False
 
-        self.cache_directory = cache_directory
+        self.cache_directory = Path(cache_directory)
+        self.cache_directory.mkdir(exist_ok=True)
 
-        if scan_id is not None:
-            self.scan_id = scan_id
-            cache_file_name = "_".join(["response", scan_id]) + ".pkl"
-            self.cache_file = Path(self.cache_directory) / Path(cache_file_name)
-        else:
-            self.cache_file = None
-            self.scan_id = None
-
-        self.scan_credentials = Credentials()
-
-        if self.cache_file is not None and self.cache_file.exists():
+        if not ignore_cache:
             self.read_from_cache()
-        else:
+
+        self.urls_to_scan = list(set(urls_to_scan).difference(set(self.domains.keys())))
+
+        if self.urls_to_scan:
+            self.scan_credentials = Credentials()
             self.start_url_scan()
             self.wait_until_done()
             self.get_results()
+
+        self.export_results()
 
     def start_url_scan(self):
 
@@ -109,7 +118,7 @@ class InternetNlScanner(object):
             type=self.scan_type,
             tracking_information=self.tracking_information,
             name=self.scan_name,
-            domains=self.domains,
+            domains=self.urls_to_scan,
         )
         response = requests.post(f'{self.api_url}/requests',
                                  json=post_parameters,
@@ -169,25 +178,65 @@ class InternetNlScanner(object):
 
     def read_from_cache(self):
 
-        _logger.info(f"Reading response scan {self.scan_id} from cache {self.cache_file}")
-        with open(str(self.cache_file), "rb") as stream:
-            self.scan_results = pickle.load(stream)
+        cache_files = glob.glob(f"{self.cache_directory}/*pkl")
+        if cache_files:
+            for cache_file in cache_files:
+                _logger.info(f"Reading response scan cache {cache_file}")
+                with open(str(cache_file), "rb") as stream:
+                    domains = pickle.load(stream)
+                for url, scan_result in domains.items():
+                    self.domains[url] = scan_result
 
     def get_results(self):
 
-        self.scan_results = requests.get(f"{self.api_url}/requests/{self.scan_id}/results",
-                                         auth=self.scan_credentials.http_auth)
-        self.scan_results.raise_for_status()
+        response = requests.get(f"{self.api_url}/requests/{self.scan_id}/results",
+                                auth=self.scan_credentials.http_auth)
+        response.raise_for_status()
 
-        with open(str(self.cache_file), "wb") as stream:
-            pickle.dump(self.scan_results, stream)
+        scan_results = response.json()
+
+        domains = scan_results["domains"]
+
+        cache_file = make_cache_file_name(self.cache_directory, self.scan_id)
+        with open(str(cache_file), "wb") as stream:
+            pickle.dump(domains, stream)
+
+        for url, scan_result in domains.items():
+            self.domains[url] = scan_result
 
     def export_results(self):
 
-        api_response = self.scan_results.json()
-        request_info = api_response["request"]
-        domains = request_info["domains"]
-        _logger.info(domains)
+        _logger.info(f"Writing to {self.output_filename}")
+
+        # per table maken we een platte dict
+        tables = dict()
+        for domain, properties in self.domains.items():
+            for table_key, table_prop in properties.items():
+                if table_key not in tables.keys():
+                    tables[table_key] = dict()
+                if isinstance(table_prop, dict):
+                    new_dict = dict()
+                    for prop_key, prop_val in table_prop.items():
+                        flatten_dict(prop_key, prop_val, new_dict)
+                    tables[table_key][domain] = new_dict
+                else:
+                    tables[table_key][domain] = table_prop
+
+        connection = sqlite3.connect(self.output_filename)
+        for table_key, table_prop in tables.items():
+            dataframe = pd.DataFrame.from_dict(table_prop, orient='index')
+            dataframe.to_sql(table_key, con=connection, if_exists="replace")
+
+
+def flatten_dict(current_key, current_value, new_dict):
+    """ gegeven de current key en value van een dict, zet de value als een string, of als een
+    dict maak een nieuwe key gebaseerd of the huidige key en dict key """
+    if isinstance(current_value, dict):
+        for key, value in current_value.items():
+            new_key = "_".join([current_key, key])
+            flatten_dict(new_key, value, new_dict)
+    else:
+        new_dict[current_key] = current_value
 
 
 def parse_args(args):
@@ -210,6 +259,9 @@ def parse_args(args):
     parser.add_argument("--api_url", help="Api URL. If not given, default is taken")
     parser.add_argument("--domain_file", action="store")
     parser.add_argument("--url", action="append", nargs="*")
+    parser.add_argument("--output_filename", action="store", help="Output file",
+                        default="internet_nl.sqlite")
+    parser.add_argument("--ignore_cache", action="store_true", help="Do not read cache")
 
     parsed_arguments = parser.parse_args(args)
 
@@ -222,16 +274,20 @@ def main(argv):
 
     _logger.setLevel(args.loglevel)
 
-    domains = list()
+    urls_to_scan = list()
     if args.domain_file is not None:
         _logger.info(f"Reading urls from {args.domain_file}")
-        domains = pd.read_csv(args.domain_file)
+        urls = pd.read_csv(args.domain_file)
+        urls_to_scan.extend(urls)
 
     if args.url is not None:
         for urls in args.url:
-            domains.append(urls[0])
+            urls_to_scan.append(urls[0])
 
-    internet = InternetNlScanner(domains=urls)
+    internet = InternetNlScanner(urls_to_scan=urls_to_scan,
+                                 ignore_cache=args.ignore_cache,
+                                 output_filename=args.output_filename
+                                 )
 
 
 if __name__ == "__main__":
