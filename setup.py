273f0c996ff8ea88e19dbbee42be39fafb8f9c46
Eelco van Vliet <>
2022-11-25 11:24:17 +0100

    updated for pyscaffold

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
     Setup file for internetnl_be_scan.
     Use setup.cfg to configure your project.
 
-    This file was generated with PyScaffold 4.1.5.
+    This file was generated with PyScaffold 4.3.1.
     PyScaffold helps you to put up the scaffold of your new Python project.
     Learn more under: https://pyscaffold.org/
 """
